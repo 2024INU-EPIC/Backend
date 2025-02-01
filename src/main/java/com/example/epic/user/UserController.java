373c8e25a9f2319a6e2141d1348cc61120879f4d
지호 박 <simbap2000@gmail.com>
2025-02-01 16:36:40 +0900

    메인페이지 사용자 정보 넘기기 구현

--- src/main/java/com/example/epic/user/UserController.java
+++ src/main/java/com/example/epic/user/UserController.java
@@ -1,7 +1,13 @@
 package com.example.epic.user;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +15,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+// TODO : html에 직접 접근하는 방식에서 RESTapi 연동 방식으로 바꾸기
 
 @RequiredArgsConstructor
 @Controller
@@ -36,11 +46,26 @@ public class UserController {
         userService.create(userCreateForm.getUsername(),
                 userCreateForm.getEmail(), userCreateForm.getPassword1());
 
-        return "redirect:/";
+        return "redirect:/user/main";
     }
     @GetMapping("/login")
     public String login(){
         return "login_form";
     }
 
+    @GetMapping("/dashboard")
+    public ResponseEntity<Optional<SiteUser>> dashboard(@AuthenticationPrincipal UserDetails userDetails) {
+        Optional<SiteUser> user = userService.getByUsername(userDetails.getUsername());
+
+        // TODO: user 정보 기반으로 단어장 db랑 학습 통계 db에 접근
+
+        // TODO: 받아온 정보들을 json 파일 형식으로 넘겨주기
+        return ResponseEntity.ok(user);
+    }
+
+    // TODO: /user/profile 구현하기
+    @GetMapping("/user/mypage")
+    public String userPage(@AuthenticationPrincipal UserDetails userDetails) {
+        return "user_page";
+    };
 }
--- src/main/java/com/example/epic/user/UserService.java
+++ src/main/java/com/example/epic/user/UserService.java
@@ -1,8 +1,11 @@
 package com.example.epic.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -19,5 +22,19 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(password));
         this.userRepository.save(user);
             return user;
+    }
+
+    public Optional<SiteUser> getByUsername(String username) {
+        Optional<SiteUser> _siteUser = this.userRepository.findByusername(username);
+        if (_siteUser.isEmpty()) {
+            // 이메일과 사용자 이름 모두 없는 경우에 대한 예외 처리
+            if (username.contains("@")) {
+                throw new   UsernameNotFoundException("입력한 이메일로 사용자를 찾을 수 없습니다.");
+            } else {
+                throw new UsernameNotFoundException("입력한 사용자 이름으로 사용자를 찾을 수 없습니다.");
+            }
+        }
+        SiteUser siteUser = _siteUser.get();
+        return Optional.of(siteUser);
     }
 }
